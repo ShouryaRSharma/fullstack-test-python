8ec3fc3349ce3634aabf2201915c40ac92a0889f
ShouryaRSharma <0shouryasharma0@gmail.com>
2024-04-05 15:19:50 +0100

    test: :test_tube: Added failing get_total_income test

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from typing import Any, Dict, Union
+
+
 def normalise_transactions(transactions):
+    pass
+
+
+def get_total_income(transactions: Dict[str, Any]) -> Dict[str, Union[float, int]]:
+    """Get total of and number of positive transaction amounts.
+    
+    Args:
+        transactions (Dict[str, Any]): Dictionary object holding transaction data
+
+    Returns:
+        Dictionary object holding total income and number of transactions
+    """
     pass
--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ from csv import DictReader
 
 import pytest
 
-from main import normalise_transactions
+from main import normalise_transactions, get_total_income
 
 
 @pytest.fixture
@@ -12,5 +12,16 @@ def transactions():
 
 
 def test_normalise_transactions(transactions):
-    normalise_transactions(transactions)
-    assert False
+    # TODO: Write unit test
+    pass
+
+
+def test_get_total_income():
+    transactions = [
+        {'amount': '700.00', 'category': 'Salary'},
+        {'amount': '100.00', 'category': 'Capital Gains'},
+        {'amount': '-100.0', 'category': 'Shopping'},
+    ]
+    result = get_total_income(transactions)
+    assert result['total_income'] == 800.00
+    assert result['num_transactions'] == 2
