59e84f51c03380f460499daa5b0e29152f2b8626
ShouryaRSharma <0shouryasharma0@gmail.com>
2024-04-05 16:54:53 +0100

    feat: :sparkles: Updated normalise function to analyse transaction data in one go

--- main.py
+++ main.py
@@ -1,48 +1,53 @@
 from typing import List, Dict, Union
 from models import Transaction, CategoryInfo
 
-
-def normalise_transactions(transactions):
-    pass
-
-
-def get_total_amount(transactions: List[Transaction]) -> Dict[str, Union[float, int]]:
-    """Get sum of and number of positive and negative transaction amounts.
+def normalise_transactions(
+    transactions: List[Transaction]
+) -> Dict[str, Union[float, int, Dict[str, CategoryInfo]]]:
+    """Analyse transaction data to get total income, spending, and group by categories.
 
     Args:
         transactions (List[Transaction]): List of transaction dictionaries
 
     Returns:
-        Dictionary object holding total income, total spending, and number of transactions
+        Dictionary object holding total income, total spending, num transactions and category info on spending and salary.
     """
     total_income = 0.0
     total_spending = 0.0
     num_income_transactions = 0
     num_spending_transactions = 0
+    spending_by_category: Dict[str, CategoryInfo] = {}
+    income_by_payer_for_salary: Dict[str, CategoryInfo] = {}
 
     for transaction in transactions:
-        amount = float(transaction['amount'])
+        amount = round(float(transaction['amount']), 2)
+        category = transaction["category"]
+
         if amount > 0:
             total_income += amount
             num_income_transactions += 1
+            if category == "Salary":
+                income_by_payer_for_salary = get_income_by_payer_for_salary(
+                    transaction, income_by_payer_for_salary)
         elif amount < 0:
             total_spending += amount
             num_spending_transactions += 1
+            spending_by_category = get_spending_by_category(transaction, spending_by_category)
 
     return {
         'total_income': total_income,
         'total_spending': total_spending,
         'num_income_transactions': num_income_transactions,
-        'num_spending_transactions': num_spending_transactions
+        'num_spending_transactions': num_spending_transactions,
+        'spending_by_category': spending_by_category,
+        'income_by_payer_for_salary': income_by_payer_for_salary
     }
 
-
 def get_spending_by_category(
-    transaction: Transaction, 
-    category_dict: Dict[str, CategoryInfo]
+    transaction: Transaction, category_dict: Dict[str, CategoryInfo]
 ) -> Dict[str, CategoryInfo]:
     """Get spending info for transaction and update category dictionary with count.
-    
+
     Args:
         transaction (Transaction): Transaction from which we get spending info
         category_dict (Dict[str, CategoryInfo]): Dictionary to hold spending by category info
@@ -52,21 +57,21 @@ def get_spending_by_category(
     """
     amount = float(transaction['amount'])
     category = transaction['category']
-
     if category not in category_dict:
-        category_dict[category] = {'amount': 0.0, 'count': 0}
+        category_dict[category] = {'amount': 0.00, 'count': 0}
     category_dict[category]['amount'] += amount
     category_dict[category]['count'] += 1
-
+    
+    # Round the amount to two decimal places
+    category_dict[category]['amount'] = round(category_dict[category]['amount'], 2)
+    
     return category_dict
 
-
 def get_income_by_payer_for_salary(
-    transaction: Transaction,
-    salary_dict: Dict[str, CategoryInfo]
+    transaction: Transaction, salary_dict: Dict[str, CategoryInfo]
 ) -> Dict[str, CategoryInfo]:
     """Get salary info per payer and update salary dictionary with count.
-    
+
     Args:
         transaction (Transaction): Transaction from which we get salary info
         salary_dict (Dict[str, CategoryInfo]): Dictionary to hold salary by payer
@@ -74,12 +79,14 @@ def get_income_by_payer_for_salary(
     Returns:
         Updated salary dict with new amount and count number
     """
-    amount = float(transaction['amount'])
+    amount = float(f"{float(transaction['amount']):.2f}")
     description = transaction['description']
 
     if description not in salary_dict:
-        salary_dict[description] = {'amount': 0.0, 'count': 0}
+        salary_dict[description] = {'amount': 0.00, 'count': 0}
+
     salary_dict[description]['amount'] += amount
     salary_dict[description]['count'] += 1
-    
+    salary_dict[description]['amount'] = round(salary_dict[description]['amount'], 2)
+
     return salary_dict
--- test_main.py
+++ test_main.py
@@ -1,32 +1,14 @@
 from csv import DictReader
+from decimal import Decimal
 import pytest
 
-from main import normalise_transactions, get_total_amount, get_spending_by_category, get_income_by_payer_for_salary
+from main import normalise_transactions, get_spending_by_category, get_income_by_payer_for_salary
 
 
 @pytest.fixture
 def transactions():
     with open("transactions.csv") as f:
         yield list(DictReader(f))
-
-
-def test_normalise_transactions(transactions):
-    # TODO: Write unit test
-    pass
-
-
-def test_get_total_amount():
-    transactions = [
-        {'amount': '1500.0', 'category': 'Salary', 'description': 'ABC Company Salary'},
-        {'amount': '-100.0', 'category': 'Shopping', 'description': 'Grocery Shopping'},
-        {'amount': '200.0', 'category': 'Other Income', 'description': 'Freelance Work'},
-        {'amount': '-50.0', 'category': 'Bills', 'description': 'Electricity Bill'},
-    ]
-    result = get_total_amount(transactions)
-    assert result['total_income'] == 1700.0
-    assert result['total_spending'] == -150.0
-    assert result['num_income_transactions'] == 2
-    assert result['num_spending_transactions'] == 2
 
 
 def test_get_spending_by_category():
@@ -39,3 +21,15 @@ def test_get_income_by_payer_for_salary():
     transaction = {'amount': '1500.00', 'category': 'Salary', 'description': 'WEB GENIUS'}
     result = get_income_by_payer_for_salary(transaction, {})
     assert result['WEB GENIUS'] == {'amount': 1500.0, 'count': 1}
+
+
+def test_normalise_transactions(transactions):
+    result = normalise_transactions(transactions)
+    assert result['total_income'] == 5023.95
+    assert result['total_spending'] == -325.31
+    assert result['num_income_transactions'] == 5
+    assert result['num_spending_transactions'] == 11
+    assert result['spending_by_category']['Shopping'] == {'amount': -136.30, 'count': 5}
+    assert result['spending_by_category']['Food & Dining'] == {'amount': -17.40, 'count': 1}
+    assert result['spending_by_category']['Bills and Utilities'] == {'amount': -69.86, 'count': 2}
+    assert result['income_by_payer_for_salary']['WEB GENIUS'] == {'amount': 4700.00, 'count': 3}
